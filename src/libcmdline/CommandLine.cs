d6a9324d33bff5035aaa25e4ffd5d3656a4a25d7
Alexander Fast <mizipzor@gmail.com>
2013-01-09 22:52:48 +0100

    Added ResourcedOptionAttribute.

--- src/libcmdline/CommandLine.cs
+++ src/libcmdline/CommandLine.cs
@@ -45,6 +45,7 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
+using System.Resources;
 using System.Runtime.Serialization;
 using System.Threading;
 using CommandLine.Internal;
@@ -1569,6 +1570,61 @@ namespace CommandLine
         }
 
         private const string DefaultHelpText = "Display this help screen.";
+    }
+
+    /// <summary>
+    /// Models an option specification where the HelpText is in a resource file.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class ResourcedOptionAttribute : OptionAttribute
+    {
+        /// <summary>
+        /// The resource manager used to retrieve the resourced strings.
+        /// </summary>
+        public static ResourceManager ResourceManager;
+
+        private string helpTextKey;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommandLine.ResourcedOptionAttribute"/> class.
+        /// </summary>
+        /// <param name="shortName">The short name of the option or null if not used.</param>
+        /// <param name="longName">The long name of the option or null if not used.</param>
+        public ResourcedOptionAttribute(char shortName, string longName)
+            : base(shortName, longName)
+        {
+        }
+
+        /// <summary>
+        /// This is the name of the string resource in the ResourceManager that should be used as HelpText.
+        /// </summary>
+        public string HelpTextKey
+        {
+            get { return helpTextKey; }
+            set
+            {
+                if (ResourceManager == null)
+                {
+                    throw new InvalidOperationException("ResourcedOption: You need to assign a ResourceManager.");
+                }
+                helpTextKey = value;
+                base.HelpText = ResourceManager.GetString(value);
+            }
+        }
+
+        /// <summary>
+        /// A short description of this command line option. Usually a sentence summary.
+        /// You are not allowed to set this on <see cref="CommandLine.ResourcedOptionAttribute"/>.
+        /// Instead, use HelpTextKey and ResourceManager.
+        /// </summary>
+        public new string HelpText
+        {
+            get { return base.HelpText; }
+            set
+            {
+                throw new InvalidOperationException("ResourcedOption: Specify HelpTextKey instead.");
+            }
+        }
     }
     #endregion
 
