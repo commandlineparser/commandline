919a8f0436beeaa05479a2c49e0ec9dec1463f87
gsscoder <gsscoder@gmail.com>
2013-02-21 14:22:08 +0100

    Finally fixed issue #6.

--- src/tests/Unit/Attributes/OptionArrayAttributeFixture.cs
+++ src/tests/Unit/Attributes/OptionArrayAttributeFixture.cs
@@ -3,17 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Xunit;
+using CommandLine.Tests.Fakes;
+using FluentAssertions;
 
 namespace CommandLine.Tests.Unit.Attributes
 {
-
-
     public class OptionArrayAttributeFixture
     {
-        //[Fact]
+        /// <summary>
+        /// https://github.com/gsscoder/commandline/issues/6
+        /// </summary>
+        [Fact]
         public void Should_correctly_parse_two_consecutive_arrays()
         {
-            // TODO
+            // Given
+            var options = new OptionsWithTwoArrays();
+            var parser = new Parser();
+            var argumets = new[] { "--source", @"d:/document.docx", "--output", @"d:/document.xlsx",
+                    "--headers", "1", "2", "3", "4",              // first array
+                    "--content", "5", "6", "7", "8", "--verbose"  // second array
+                };
+
+            // When
+            var result = parser.ParseArguments(argumets, options);
+
+            // Than
+            result.Should().BeTrue();
+            options.Headers.Should().HaveCount(c => c == 4);
+            options.Headers.Should().ContainInOrder(new uint[] { 1, 2, 3, 4 });
+            options.Content.Should().HaveCount(c => c == 4);
+            options.Content.Should().ContainInOrder(new uint[] { 5, 6, 7, 8 });
         }
     }
 }
