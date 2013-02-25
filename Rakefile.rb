42f841941036a475a51d55094ccaf4cc5e92fe26
gsscoder <gsscoder@gmail.com>
2013-02-25 07:56:14 +0100

    Upgrading build version.

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,6 +1,6 @@
 PRODUCT = "Command Line Parser Library"
 DESCRIPTION = "The Command Line Parser Library offers to CLR applications a clean and concise API for manipulating command line arguments and related tasks."
-VERSION = "1.9.62.1"
+VERSION = "1.9.62.11"
 INF_VERSION = "1.9.62-rc1"
 AUTHOR = "Giacomo Stelluti Scala"
 COPYRIGHT = "Copyright (c) 2005 - 2013 " + AUTHOR
