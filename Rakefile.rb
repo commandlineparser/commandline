0818eecb63781556440304728c4a0241cf4fa905
gsscoder <gsscoder@gmail.com>
2013-02-27 17:54:31 +0100

    Fixing version numeration error.

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,7 +1,7 @@
 PRODUCT = "Command Line Parser Library"
 DESCRIPTION = "The Command Line Parser Library offers to CLR applications a clean and concise API for manipulating command line arguments and related tasks."
-VERSION = "1.9.7.2"
-INF_VERSION = "1.9.7-stable"
+VERSION = "1.9.71.2"
+INF_VERSION = "1.9.71-stable"
 AUTHOR = "Giacomo Stelluti Scala"
 COPYRIGHT = "Copyright (c) 2005 - 2013 " + AUTHOR
 LICENSE_URL = "https://raw.github.com/gsscoder/commandline/master/doc/LICENSE"
