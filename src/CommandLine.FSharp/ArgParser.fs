8235bcae5237aa65b86db5290a2990a55e138987
gsscoder <gsscoder@gmail.com>
2013-04-29 11:07:20 +0200

    First layout of FSharp API.

--- src/CommandLine.FSharp/ArgParser.fs
+++ src/CommandLine.FSharp/ArgParser.fs
@@ -15,23 +15,26 @@ type public ParserConfig = {
 }
 
 module ArgParser =
-    let ParseOptions<'a when 'a : (new : unit -> 'a)> (config, args) =
-        let getComparer config =
-            if config.CaseSensitive then StringComparer.Ordinal
-            else StringComparer.OrdinalIgnoreCase
+    let private getComparer config =
+        if config.CaseSensitive then StringComparer.Ordinal
+        else StringComparer.OrdinalIgnoreCase
+    let private parse<'a>(parseFunc : unit -> ParserResult<'a>, config) =
         let filterUnknown (result: ParserResult<'a>) =
             match config.IgnoreUnknownArguments with
                 | true -> ParserResult(result.Tag, result.Value, query { for err in result.Errors do
-                                                                         where(err.Tag <> ErrorType.UnknownOptionError)
-                                                                         select err }, result.VerbTypes)
+                                                                            where(err.Tag <> ErrorType.UnknownOptionError)
+                                                                            select err }, result.VerbTypes)
                 | _ -> result
         let displayHelp (result: ParserResult<'a>) =
             match config.HelpWriter with
                 | null -> result
                 | _ -> config.HelpWriter.Write(HelpText.AutoBuild(result)) |> ignore; result
-        InstanceBuilder.Build(Func<'a>(fun () -> new 'a()), args, getComparer(config), config.ParsingCulture)
+        parseFunc()
             |> filterUnknown
             |> displayHelp
 
-//    let ParseVerbs args =
-//        args
+    let ParseOptions<'a when 'a : (new : unit -> 'a)> (config, args) =
+        parse((fun () -> InstanceBuilder.Build(Func<'a>(fun () -> new 'a()), args, getComparer(config), config.ParsingCulture)), config)
+
+    let ParseVerbs (config, args, types) =
+        parse((fun () -> InstanceChooser.Choose(types, args, getComparer(config), config.ParsingCulture)), config)
