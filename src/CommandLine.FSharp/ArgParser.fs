e00dd9f0863104cd6dba73c549db3c50eb23f732
gsscoder <gsscoder@gmail.com>
2013-05-05 09:25:46 +0200

    Adding EnableDashDash to FSharp API.

--- src/CommandLine.FSharp/ArgParser.fs
+++ src/CommandLine.FSharp/ArgParser.fs
@@ -11,6 +11,7 @@ type public ParserConfig = {
     CaseSensitive : bool;
     HelpWriter : TextWriter;
     IgnoreUnknownArguments : bool;
+    EnableDashDash : bool;
     ParsingCulture : CultureInfo;
 }
 
@@ -28,13 +29,18 @@ module ArgParser =
         let displayHelp result =
             match config.HelpWriter with
                 | null -> result
-                | _ -> config.HelpWriter.Write(HelpText.AutoBuild(result)) |> ignore; result
+                | _ -> config.HelpWriter.Write(HelpText.AutoBuild result) |> ignore; result
         parseFunc()
             |> filterUnknown
             |> displayHelp
+    let private tokenize args optionSpecs config =
+        if config.EnableDashDash then
+            Tokenizer.PreprocessDashDash(args, Func<seq<string>, StatePair<seq<Token>>>(fun a -> Tokenizer.Tokenize(a, Func<string,bool>(fun name -> NameLookup.Contains(name, optionSpecs,  getComparer config)))))
+        else
+            Tokenizer.Tokenize(args, Func<string,bool>(fun name -> NameLookup.Contains(name, optionSpecs,  getComparer config)))
 
     let ParseOptions<'a when 'a : (new : unit -> 'a)> config args =
-        parse((fun () -> InstanceBuilder.Build(Func<'a>(fun () -> new 'a()), args, getComparer(config), config.ParsingCulture)), config)
+        parse((fun () -> InstanceBuilder.Build(Func<'a>(fun () -> new 'a()), Func<seq<string>,seq<OptionSpecification>,StatePair<seq<Token>>>(fun ar os -> tokenize ar os config), args, getComparer config, config.ParsingCulture)), config)
 
     let ParseVerbs config args types =
-        parse((fun () -> InstanceChooser.Choose(types, args, getComparer(config), config.ParsingCulture)), config)
+        parse((fun () -> InstanceChooser.Choose(Func<seq<string>,seq<OptionSpecification>,StatePair<seq<Token>>>(fun ar os -> tokenize ar os config), types, args, getComparer config, config.ParsingCulture)), config)
