4100a873e5985302a4017ebd7b8c62f4926d5ed4
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-06-12 16:47:11 +0200

    Adding a test for ExplodeOptionList()

--- src/CommandLine.Tests/Unit/Core/TokenizerTests.cs
+++ src/CommandLine.Tests/Unit/Core/TokenizerTests.cs
@@ -14,7 +14,7 @@ namespace CommandLine.Tests.Unit.Core
     public class TokenizerTests
     {
         [Fact]
-        public void Explode_scalar_with_separator_returns_sequence()
+        public void Explode_scalar_with_separator_in_odd_args_input_returns_sequence()
         {
             // Fixture setup
             var expectedTokens = new[] { Token.Name("i"), Token.Value("10"), Token.Name("string-seq"),
@@ -30,7 +30,30 @@ namespace CommandLine.Tests.Unit.Core
                             Token.Name("string-seq"), Token.Value("aaa,bb,cccc"), Token.Name("switch") }),
                         Enumerable.Empty<Error>()),
                         optionName => NameLookup.WithSeparator(optionName, specs, StringComparer.InvariantCulture));
-            //optionName => "string-seq".EqualsOrdinal(optionName) ? Maybe.Just(",") : Maybe.Nothing<string>());
+
+            // Verify outcome
+            Assert.True(expectedTokens.SequenceEqual(result.Value));
+
+            // Teardown
+        }
+
+        [Fact]
+        public void Explode_scalar_with_separator_in_even_args_input_returns_sequence()
+        {
+            // Fixture setup
+            var expectedTokens = new[] { Token.Name("x"), Token.Name("string-seq"),
+                Token.Value("aaa"), Token.Value("bb"),  Token.Value("cccc"), Token.Name("switch"), };
+            var specs = new[] { new OptionSpecification(string.Empty, "string-seq",
+                false, string.Empty, -1, -1, ",", null, typeof(IEnumerable<string>), string.Empty, string.Empty, new List<string>())};
+
+            // Exercize system
+            var result =
+                Tokenizer.ExplodeOptionList(
+                    StatePair.Create(
+                        Enumerable.Empty<Token>().Concat(new[] { Token.Name("x"),
+                            Token.Name("string-seq"), Token.Value("aaa,bb,cccc"), Token.Name("switch") }),
+                        Enumerable.Empty<Error>()),
+                        optionName => NameLookup.WithSeparator(optionName, specs, StringComparer.InvariantCulture));
 
             // Verify outcome
             Assert.True(expectedTokens.SequenceEqual(result.Value));
