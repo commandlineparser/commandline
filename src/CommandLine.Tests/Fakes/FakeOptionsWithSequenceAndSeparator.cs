cde6d734775f849c5c0b3f6848a332b0ba39ce3c
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-06-13 18:46:35 +0200

    Removing range from fake type, tests green

--- src/CommandLine.Tests/Fakes/FakeOptionsWithSequenceAndSeparator.cs
+++ src/CommandLine.Tests/Fakes/FakeOptionsWithSequenceAndSeparator.cs
@@ -7,7 +7,7 @@ namespace CommandLine.Tests.Fakes
 {
     class FakeOptionsWithSequenceAndSeparator
     {
-        [Option("long-seq", Min=0, Max=100, Separator=';')] //todo: problem omitting min/max -> defauly should be no check
+        [Option("long-seq", Separator=';')]
         public IEnumerable<long> LongSequence { get; set; }
 
         [Option('s', Min = 0, Max = 100, Separator = ',')]
