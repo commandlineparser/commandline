f7a23eada46b23e8c6cfd4cf1e1b7d6498359a43
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-06-14 14:45:40 +0200

    Adding XUnit Test target

--- build.fsx
+++ build.fsx
@@ -1,25 +1,42 @@
 ﻿#r "packages/FAKE.3.35.1/tools/FakeLib.dll"
 open Fake
 
+//RestorePackages()
+
 let buildDir = "./build/"
+let testDir = "./build/test/"
 
 Target "Clean" (fun _ ->
-    CleanDir buildDir
+    CleanDirs [buildDir; testDir]
 )
 
 Target "Default" (fun _ ->
     trace "Command Line Parser Library 2.0 pre-release"
 )
 
-Target "BuildLib"(fun _ ->
-    !! "src/**/*.csproj"
+Target "BuildLib" (fun _ ->
+    !! "src/CommandLine/CommandLine.csproj"
         |> MSBuildRelease buildDir "Build"
-        |> Log "AppBuild-Output: "
+        |> Log "LibBuild-Output: "
+)
+
+Target "BuildTest" (fun _ ->
+    !! "src/CommandLine.Tests/CommandLine.Tests.csproj"
+        |> MSBuildDebug testDir "Build"
+        |> Log "TestBuild-Output: "
+)
+
+Target "Test" (fun _ ->
+    trace "Running Tests..."
+    !! (testDir + @"\CommandLine.Tests.dll") 
+      |> xUnit (fun p -> {p with OutputDir = testDir})
 )
 
 // Dependencies
 "Clean"
     ==> "BuildLib"
+    ==> "BuildTest"
+    ==> "Test"
     ==> "Default"
 
 RunTargetOrDefault "Default"
