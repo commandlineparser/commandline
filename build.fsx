0b27c9f276d79364fb03e5a39dd69fdce1750275
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-06-14 13:55:49 +0200

    First stub of FAKE build script

---
+++ build.fsx
@@ -0,0 +1,18 @@
+﻿#r "packages/FAKE.3.35.1/tools/FakeLib.dll"
+open Fake
+
+let buildDir = "./build/"
+
+Target "Clean" (fun _ ->
+    CleanDir buildDir
+)
+
+Target "Default" (fun _ ->
+    trace "TODO: complete"
+)
+
+// Dependencies
+"Clean"
+    ==> "Default"
+
+RunTargetOrDefault "Default"
