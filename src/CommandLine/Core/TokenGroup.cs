1e9fb2d4dcff26adc60f79603045f80768294c83
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-06-14 21:22:30 +0200

    Defining typed tuple for TokenPartioner

---
+++ src/CommandLine/Core/TokenGroup.cs
@@ -0,0 +1,38 @@
+﻿// Copyright 2005-2013 Giacomo Stelluti Scala & Contributors. All rights reserved. See doc/License.md in the project root for license information.
+
+using System.Collections.Generic;
+
+namespace CommandLine.Core
+{
+    internal class TokenGroup
+    {
+        private readonly IEnumerable<KeyValuePair<string, IEnumerable<string>>> options;
+        private readonly IEnumerable<string> values;
+        private readonly IEnumerable<Token> errors;
+
+        public TokenGroup(IEnumerable<KeyValuePair<string, IEnumerable<string>>> options,
+            IEnumerable<string> values,
+            IEnumerable<Token> errors)
+        {
+            this.options = options;
+            this.values = values;
+            this.errors = errors;
+        }
+
+        public IEnumerable<KeyValuePair<string, IEnumerable<string>>> Options
+        {
+            get { return this.options; }
+        }
+
+        public IEnumerable<string> Values
+        {
+            get { return this.values; }
+        }
+
+        public IEnumerable<Token> Errors
+        {
+            get { return this.errors; }
+        }
+
+    }
+}
