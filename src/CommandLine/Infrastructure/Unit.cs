e4af5f12ec4c3f653dc1134f72ecfae0d301e47d
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-06-16 12:35:23 +0200

    Adding FSharp-like Unit type

---
+++ src/CommandLine/Infrastructure/Unit.cs
@@ -0,0 +1,49 @@
+﻿// Copyright 2005-2015 Giacomo Stelluti Scala & Contributors. All rights reserved. See doc/License.md in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommandLine.Infrastructure
+{
+    /// <summary>
+    /// From https://github.com/Reactive-Extensions/Rx.NET/blob/master/Rx.NET/Source/System.Reactive.Core/Reactive/Unit.cs.
+    /// </summary>
+    internal struct Unit : IEquatable<Unit>
+    {
+        private static readonly Unit @default = new Unit();
+
+        public bool Equals(Unit other)
+        {
+            return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Unit;
+        }
+
+        public override int GetHashCode()
+        {
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return "()";
+        }
+
+        public static bool operator ==(Unit first, Unit second)
+        {
+            return true;
+        }
+
+        public static bool operator !=(Unit first, Unit second)
+        {
+            return false;
+        }
+
+        public static Unit Default { get { return @default; } }
+    }
+}
