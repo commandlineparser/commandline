2d7f131d65c90f423679b55aac4e9ec4aff2541a
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-06-16 07:25:17 +0200

    Updating source heading and doc/LICENSE years

--- src/CommandLine.Tests/Fakes/FakeOptionsWithSequenceAndOnlyMinConstraintAsValue.cs
+++ src/CommandLine.Tests/Fakes/FakeOptionsWithSequenceAndOnlyMinConstraintAsValue.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Copyright 2005-2015 Giacomo Stelluti Scala & Contributors. All rights reserved. See doc/License.md in the project root for license information.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
