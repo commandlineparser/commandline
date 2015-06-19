aff15bfe1f0b343698e98296ed88a9bcb868bcab
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-06-19 17:16:00 +0200

    Adding source file standard header

--- src/CommandLine/Core/KeyValuePairHelper.cs
+++ src/CommandLine/Core/KeyValuePairHelper.cs
@@ -1,8 +1,7 @@
-﻿using System;
+﻿// Copyright 2005-2015 Giacomo Stelluti Scala & Contributors. All rights reserved. See doc/License.md in the project root for license information.
+
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-
 using CommandLine.Infrastructure;
 
 namespace CommandLine.Core
