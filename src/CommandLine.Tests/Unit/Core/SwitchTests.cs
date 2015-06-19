06f8abba1ee4d1e2df79cf6e347283747c49196a
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-06-19 06:27:38 +0200

    Adding tests for Switch parationer

---
+++ src/CommandLine.Tests/Unit/Core/SwitchTests.cs
@@ -0,0 +1,53 @@
+﻿// Copyright 2005-2015 Giacomo Stelluti Scala & Contributors. All rights reserved. See doc/License.md in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using CommandLine.Core;
+using CommandLine.Infrastructure;
+
+using Xunit;
+using FluentAssertions;
+
+namespace CommandLine.Tests.Unit.Core
+{
+    public class SwitchTests
+    {
+        [Fact]
+        public void Partition_switch_values_from_empty_token_sequence()
+        {
+            var expected = new Token[] { };
+
+            var result = Switch.Partition(
+                new Token[] { },
+                name =>
+                    new[] { "x", "switch" }.Contains(name)
+                        ? Maybe.Just(TypeDescriptor.Create(TypeDescriptorKind.Boolean, Maybe.Nothing<int>()))
+                        : Maybe.Nothing<TypeDescriptor>());
+
+            expected.ShouldAllBeEquivalentTo(result);
+        }
+
+        [Fact]
+        public void Partition_switch_values()
+        {
+            var expected = new [] { Token.Name("x") };
+
+            var result = Switch.Partition(
+                new []
+                    {
+                        Token.Name("str"), Token.Value("strvalue"), Token.Value("freevalue"),
+                        Token.Name("x"), Token.Value("freevalue2")
+                    },
+                name =>
+                    new[] { "x", "switch" }.Contains(name)
+                        ? Maybe.Just(TypeDescriptor.Create(TypeDescriptorKind.Boolean, Maybe.Nothing<int>()))
+                        : Maybe.Nothing<TypeDescriptor>());
+
+            expected.ShouldAllBeEquivalentTo(result);
+        }
+    }
+}
