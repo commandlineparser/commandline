72524aa4706f0affb57b090881417910c1837364
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-06-21 18:22:59 +0200

    Drafting new test types for KeyValuePairHelper

---
+++ src/CommandLine.Tests/Unit/Core/KeyValuePairHelperTests.cs
@@ -0,0 +1,26 @@
+﻿// Copyright 2005-2015 Giacomo Stelluti Scala & Contributors. All rights reserved. See doc/License.md in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine.Core;
+using Xunit;
+using FluentAssertions;
+
+namespace CommandLine.Tests.Unit.Core
+{
+    public class KeyValuePairHelperTests
+    {
+        [Fact]
+        public void Empty_token_sequence_creates_an_empty_KeyValuePair_sequence()
+        {
+            var expected = new KeyValuePair<string, IEnumerable<string>>[] { };
+
+            var result = KeyValuePairHelper.CreateSequence(new Token[] { });
+
+            result.SequenceEqual(expected);
+        }
+    }
+}
