b48c8722d64b508e6c03560fa36a01218d286b34
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-06-30 13:30:55 +0200

    Changes in F# demo script

--- demo/fsharp-demo.fsx
+++ demo/fsharp-demo.fsx
@@ -1,6 +1,5 @@
 ﻿#r "./../src/CommandLine/bin/Debug/CommandLine.dll"
 
-open System
 open CommandLine
 
 type options = {
@@ -10,17 +9,17 @@ type options = {
     [<Value(0)>] LongValue : int64 option;
   }
 
-let longOrZero o =
+let formatLong o =
   match o with
-    | Some(v) -> v
-    | _ -> 0L
+    | Some(v) -> string v
+    | _ -> "{None}"
 
 let formatInput (o : options)  =
-    sprintf "--stringvalue: %s\n-i: %A\n -x: %b\n value: %u\n" o.StringValue (Array.ofSeq o.IntSequence) o.BoolValue (longOrZero o.LongValue)
+    sprintf "--stringvalue: %s\n-i: %A\n-x: %b\nvalue: %s\n" o.StringValue (Array.ofSeq o.IntSequence) o.BoolValue (formatLong o.LongValue)
 
 let args = fsi.CommandLineArgs.[1..]
 let parsed = Parser.Default.ParseArguments<options>(args)
 
 if Seq.isEmpty parsed.Errors
-then Console.WriteLine(formatInput parsed.Value)
+then printf "%s" (formatInput parsed.Value)
 else printf "Invalid: %A\n" args
