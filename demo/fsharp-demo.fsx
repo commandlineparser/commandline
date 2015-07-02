fd8295495f838ba2fae07305686c6bd5b94f6c92
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-07-02 17:51:35 +0200

    Ids case

--- demo/fsharp-demo.fsx
+++ demo/fsharp-demo.fsx
@@ -3,10 +3,10 @@
 open CommandLine
 
 type options = {
-    [<Option(HelpText = "Define a string value here.")>] StringValue : string;
-    [<Option('i', Min = 3, Max = 4, HelpText = "Define a int sequence here.")>] IntSequence : seq<int>;
-    [<Option('x', HelpText = "Define a boolean or switch value here.")>] BoolValue : bool;
-    [<Value(0)>] LongValue : int64 option;
+    [<Option(HelpText = "Define a string value here.")>] stringValue : string;
+    [<Option('i', Min = 3, Max = 4, HelpText = "Define a int sequence here.")>] intSequence : seq<int>;
+    [<Option('x', HelpText = "Define a boolean or switch value here.")>] boolValue : bool;
+    [<Value(0)>] longValue : int64 option;
   }
 
 let formatLong o =
@@ -15,7 +15,7 @@ let formatLong o =
     | _ -> "{None}"
 
 let formatInput (o : options)  =
-    sprintf "--stringvalue: %s\n-i: %A\n-x: %b\nvalue: %s\n" o.StringValue (Array.ofSeq o.IntSequence) o.BoolValue (formatLong o.LongValue)
+    sprintf "--stringvalue: %s\n-i: %A\n-x: %b\nvalue: %s\n" o.stringValue (Array.ofSeq o.intSequence) o.boolValue (formatLong o.longValue)
 
 let (|Parsed|Failed|) (r : ParserResult<'a>) =
   if Seq.isEmpty r.Errors then Parsed(r.Value)
