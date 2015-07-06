df7ae0058f679833696d4ec39d2813f562ab9466
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-07-06 06:40:19 +0200

    Issue #183 Fixed by @Thilas

--- nuget/readme.txt
+++ nuget/readme.txt
@@ -1,4 +1,4 @@
-Command Line Parser Library 2.0.35.0 alpha
+Command Line Parser Library 2.0.37.0 alpha
 ------------------------------------------
 Giacomo Stelluti Scala
 (gsscoder@gmail.com)
--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Resources;
 
 [assembly: AssemblyProduct("Command Line Parser Library")]
 [assembly: AssemblyCopyright("Copyright (c) 2005 - 2015 Giacomo Stelluti Scala")]
-[assembly: AssemblyVersion("2.0.35.0")]
-[assembly: AssemblyFileVersion("2.0.35.0")]
+[assembly: AssemblyVersion("2.0.37.0")]
+[assembly: AssemblyFileVersion("2.0.37.0")]
 
-[assembly: AssemblyInformationalVersion("2.0.35-alpha")]
+[assembly: AssemblyInformationalVersion("2.0.37-alpha")]
 [assembly: NeutralResourcesLanguage("en-US")]
