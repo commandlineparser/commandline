46492ecea9cead604e0767e51f60086264e8f4c6
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-07-06 10:54:01 +0200

    Drafting new lambda list class

---
+++ src/CommandLine/Core/PreprocessorGuards.cs
@@ -0,0 +1,31 @@
+﻿// Copyright 2005-2015 Giacomo Stelluti Scala & Contributors. All rights reserved. See doc/License.md in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommandLine.Infrastructure;
+
+namespace CommandLine.Core
+{
+    static class PreprocessorGuards
+    {
+        public static IEnumerable<Func<IEnumerable<string>, IEnumerable<Error>>>
+            Lookup(StringComparer nameComparer)
+        {
+            return new List<Func<IEnumerable<string>, IEnumerable<Error>>>
+                {
+                    HelpCommand(nameComparer)
+                };
+        }
+
+        public static Func<IEnumerable<string>, IEnumerable<Error>> HelpCommand(StringComparer nameComparer)
+        {
+            return
+                arguments =>
+                    nameComparer.Equals("--help", arguments.First())
+                        ? new Error[] { new HelpRequestedError() }
+                        : Enumerable.Empty<Error>();
+        }
+    }
+}
