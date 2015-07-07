4e4a99bc97e64236c8ad998de029ea314c7a5a8b
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-07-07 12:10:15 +0200

    Added Return<T1...T16>()

--- src/CommandLine/ParserResultExtensions.cs
+++ src/CommandLine/ParserResultExtensions.cs
@@ -1172,5 +1172,135 @@ namespace CommandLine
             }
             return notParsedFunc(((NotParsed<object>)result).Errors);
         }
+
+        /// <summary>
+        /// Provides a way to transform result data into another value.
+        /// </summary>
+        /// <typeparam name="T1">First verb type.</typeparam>
+        /// <typeparam name="T2">Second verb type.</typeparam>
+        /// <typeparam name="T3">Third verb type.</typeparam>
+        /// <typeparam name="T4">Fourth verb type.</typeparam>
+        /// <typeparam name="T5">Fifth verb type.</typeparam>
+        /// <typeparam name="T6">Sixth verb type.</typeparam>
+        /// <typeparam name="T7">Seventh verb type.</typeparam>
+        /// <typeparam name="T8">Eighth verb type.</typeparam>
+        /// <typeparam name="T9">Ninth verb type.</typeparam>
+        /// <typeparam name="T10">Tenth verb type.</typeparam>
+        /// <typeparam name="T11">Eleventh verb type.</typeparam>
+        /// <typeparam name="T12">Twelfth verb type.</typeparam>
+        /// <typeparam name="T13">Thirteenth verb type.</typeparam>
+        /// <typeparam name="T14">Fourteenth verb type.</typeparam>
+        /// <typeparam name="T15">Fifteenth verb type.</typeparam>
+        /// <typeparam name="T16">Sixteenth verb type.</typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="result">The result in verb scenario.</param>
+        /// <param name="parsedFunc1">Lambda executed on successful parsing of <see cref="T1"/>.</param>
+        /// <param name="parsedFunc2">Lambda executed on successful parsing of <see cref="T2"/>.</param>
+        /// <param name="parsedFunc3">Lambda executed on successful parsing of <see cref="T3"/>.</param>
+        /// <param name="parsedFunc4">Lambda executed on successful parsing of <see cref="T4"/>.</param>
+        /// <param name="parsedFunc5">Lambda executed on successful parsing of <see cref="T5"/>.</param>
+        /// <param name="parsedFunc6">Lambda executed on successful parsing of <see cref="T6"/>.</param>
+        /// <param name="parsedFunc7">Lambda executed on successful parsing of <see cref="T7"/>.</param>
+        /// <param name="parsedFunc8">Lambda executed on successful parsing of <see cref="T8"/>.</param>
+        /// <param name="parsedFunc9">Lambda executed on successful parsing of <see cref="T9"/>.</param>
+        /// <param name="parsedFunc10">Lambda executed on successful parsing of <see cref="T10"/>.</param>
+        /// <param name="parsedFunc11">Lambda executed on successful parsing of <see cref="T11"/>.</param>
+        /// <param name="parsedFunc12">Lambda executed on successful parsing of <see cref="T12"/>.</param>
+        /// <param name="parsedFunc13">Lambda executed on successful parsing of <see cref="T13"/>.</param>
+        /// <param name="parsedFunc14">Lambda executed on successful parsing of <see cref="T14"/>.</param>
+        /// <param name="parsedFunc15">Lambda executed on successful parsing of <see cref="T15"/>.</param>
+        /// <param name="parsedFunc16">Lambda executed on successful parsing of <see cref="T16"/>.</param>
+        /// <param name="notParsedFunc">Lambda executed on failed parsing.</param>
+        /// <returns>The new value.</returns>
+        public static TResult Return<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult>(this ParserResult<object> result,
+            Func<T1, TResult> parsedFunc1,
+            Func<T2, TResult> parsedFunc2,
+            Func<T3, TResult> parsedFunc3,
+            Func<T4, TResult> parsedFunc4,
+            Func<T5, TResult> parsedFunc5,
+            Func<T6, TResult> parsedFunc6,
+            Func<T7, TResult> parsedFunc7,
+            Func<T8, TResult> parsedFunc8,
+            Func<T9, TResult> parsedFunc9,
+            Func<T10, TResult> parsedFunc10,
+            Func<T11, TResult> parsedFunc11,
+            Func<T12, TResult> parsedFunc12,
+            Func<T13, TResult> parsedFunc13,
+            Func<T14, TResult> parsedFunc14,
+            Func<T15, TResult> parsedFunc15,
+            Func<T16, TResult> parsedFunc16,
+            Func<IEnumerable<Error>, TResult> notParsedFunc)
+        {
+            var parsed = result as Parsed<object>;
+            if (parsed != null)
+            {
+                if (parsed.Value.GetType() == typeof(T1))
+                {
+                    return parsedFunc1((T1)parsed.Value);
+                }
+                if (parsed.Value.GetType() == typeof(T2))
+                {
+                    return parsedFunc2((T2)parsed.Value);
+                }
+                if (parsed.Value.GetType() == typeof(T3))
+                {
+                    return parsedFunc3((T3)parsed.Value);
+                }
+                if (parsed.Value.GetType() == typeof(T4))
+                {
+                    return parsedFunc4((T4)parsed.Value);
+                }
+                if (parsed.Value.GetType() == typeof(T5))
+                {
+                    return parsedFunc5((T5)parsed.Value);
+                }
+                if (parsed.Value.GetType() == typeof(T6))
+                {
+                    return parsedFunc6((T6)parsed.Value);
+                }
+                if (parsed.Value.GetType() == typeof(T7))
+                {
+                    return parsedFunc7((T7)parsed.Value);
+                }
+                if (parsed.Value.GetType() == typeof(T8))
+                {
+                    return parsedFunc8((T8)parsed.Value);
+                }
+                if (parsed.Value.GetType() == typeof(T9))
+                {
+                    return parsedFunc9((T9)parsed.Value);
+                }
+                if (parsed.Value.GetType() == typeof(T10))
+                {
+                    return parsedFunc10((T10)parsed.Value);
+                }
+                if (parsed.Value.GetType() == typeof(T11))
+                {
+                    return parsedFunc11((T11)parsed.Value);
+                }
+                if (parsed.Value.GetType() == typeof(T12))
+                {
+                    return parsedFunc12((T12)parsed.Value);
+                }
+                if (parsed.Value.GetType() == typeof(T13))
+                {
+                    return parsedFunc13((T13)parsed.Value);
+                }
+                if (parsed.Value.GetType() == typeof(T14))
+                {
+                    return parsedFunc14((T14)parsed.Value);
+                }
+                if (parsed.Value.GetType() == typeof(T15))
+                {
+                    return parsedFunc15((T15)parsed.Value);
+                }
+                if (parsed.Value.GetType() == typeof(T16))
+                {
+                    return parsedFunc16((T16)parsed.Value);
+                }
+                throw new InvalidOperationException();
+            }
+            return notParsedFunc(((NotParsed<object>)result).Errors);
+        }
     }
 }
