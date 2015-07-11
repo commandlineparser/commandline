54a872be4f8f6da605db5241e3a3b3f38cb77ce7
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-07-11 19:18:11 +0200

    Bad extension m. invocation

--- src/CommandLine/Core/OptionMapper.cs
+++ src/CommandLine/Core/OptionMapper.cs
@@ -31,7 +31,7 @@ namespace CommandLine.Core
                                                 Maybe.Nothing<Error>()),
                                             Tuple.Create<SpecificationProperty, Maybe<Error>>(
                                                 pt,
-                                                Maybe.Just<Error>(new BadFormatConversionError(NameExtensions.FromOptionSpecification((OptionSpecification)pt.Specification))))),
+                                                Maybe.Just<Error>(new BadFormatConversionError(((OptionSpecification)pt.Specification).FromOptionSpecification())))),
                                 Tuple.Create(pt, Maybe.Nothing<Error>()))
                 );
             return StatePair.Create(
