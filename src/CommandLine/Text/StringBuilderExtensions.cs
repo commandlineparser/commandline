0771524e62b57e05a0307022114cfd4cddb2bc96
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-07-12 20:05:45 +0200

    GetLength -> SafeLength extension m.

--- src/CommandLine/Text/StringBuilderExtensions.cs
+++ src/CommandLine/Text/StringBuilderExtensions.cs
@@ -8,6 +8,10 @@ namespace CommandLine.Text
     static class StringBuilderExtensions
     {        public static StringBuilder AppendWhen(this StringBuilder builder, bool condition, params string[] values)        {
             if (condition)            {                foreach (var value in values)                {                    builder.Append(value);                }            }            return builder;        }
-        public static string SafeToString(this StringBuilder builder)        {            if (builder == null)            {                return string.Empty;            }            return builder.ToString();        }
-    }
+        public static string SafeToString(this StringBuilder builder)        {            return builder == null ? string.Empty : builder.ToString();        }
+
+        private static int SafeLength(this StringBuilder builder)
+        {
+            return builder == null ? 0 : builder.Length;
+        }    }
 }
