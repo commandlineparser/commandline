24de5235b03c4149960b47c364dcdb3e184e23f3
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-07-21 06:38:19 +0200

    Adjusting namespace declarations

--- src/CommandLine/Core/ArgumentsExtension.cs
+++ src/CommandLine/Core/ArgumentsExtension.cs
@@ -3,8 +3,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using CommandLine.Infrastructure;
+using CSharpx;
 
 namespace CommandLine.Core
 {
--- src/CommandLine/Core/KeyValuePairHelper.cs
+++ src/CommandLine/Core/KeyValuePairHelper.cs
@@ -2,7 +2,7 @@
 
 using System.Collections.Generic;
 using System.Linq;
-using CommandLine.Infrastructure;
+using CSharpx;
 
 namespace CommandLine.Core
 {
