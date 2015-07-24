13feb997bfd5cc59a97e312d06448c75fa3651ab
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-07-24 06:33:53 +0200

    Adding file for UnParser ext. methods

---
+++ src/CommandLine/UnParser.cs
@@ -0,0 +1,13 @@
+﻿// Copyright 2005-2015 Giacomo Stelluti Scala & Contributors. All rights reserved. See doc/License.md in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommandLine
+{
+    public static class UnParser
+    {
+    }
+}
