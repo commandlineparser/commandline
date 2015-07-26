3a67a730f2edc383a759391bf4ccc5e5174d8176
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-07-26 13:11:35 +0200

    Adding ResultExtensions class

---
+++ src/CommandLine/Infrastructure/ResultExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RailwaySharp.ErrorHandling;
+
+namespace CommandLine.Infrastructure
+{
+    static class ResultExtensions
+    {
+        public static IEnumerable<TMessage> Messages<TSuccess, TMessage>(this Result<TSuccess, TMessage> result)
+        {
+            if (result.Tag == ResultType.Ok)
+            {
+                var ok = (Ok<TSuccess, TMessage>)result;
+                return ok.Value.Messages;
+            }
+            var bad = (Bad<TSuccess, TMessage>)result;
+            return bad.Messages;
+        }
+    }
+}
