f5fd9db8832fb011b3b5e454e2a97526b6a607a2
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-07-27 19:03:09 +0200

    Reducing braces and code formatting

--- src/CommandLine/Infrastructure/StringBuilderExtensions.cs
+++ src/CommandLine/Infrastructure/StringBuilderExtensions.cs
@@ -7,58 +7,56 @@ namespace CommandLine.Infrastructure
 {
     static class StringBuilderExtensions
     {        public static StringBuilder AppendWhen(this StringBuilder builder, bool condition, params string[] values)        {
-            if (condition)            {                foreach (var value in values)                {                    builder.Append(value);                }            }            return builder;        }
+            if (condition)                foreach (var value in values)                    builder.Append(value);            return builder;        }
 
         public static StringBuilder AppendWhen(this StringBuilder builder, bool condition, params char[] values)
         {
             if (condition)
-            {
                 foreach (var value in values)
-                {
                     builder.Append(value);
-                }
-            }
+
             return builder;
         }
 
         public static StringBuilder AppendFormatWhen(this StringBuilder builder, bool condition, string format, params object[] args)
         {
-            if (condition)
-            {
-                builder.AppendFormat(format, args);
-            }
-            return builder;
+            return condition
+                ? builder.AppendFormat(format, args)
+                : builder;
         }
 
         public static StringBuilder AppendIf(this StringBuilder builder, bool condition, string ifTrue, string ifFalse)
         {
-            return condition ? builder.Append(ifTrue) : builder.Append(ifFalse);
+            return condition
+                ? builder.Append(ifTrue)
+                : builder.Append(ifFalse);
         }
 
         public static StringBuilder BimapIf(this StringBuilder builder, bool condition,
             Func<StringBuilder, StringBuilder> ifTrue, Func<StringBuilder, StringBuilder> ifFalse)
         {
-            return condition ? ifTrue(builder) : ifFalse(builder);
+            return condition
+                ? ifTrue(builder)
+                : ifFalse(builder);
         }
 
         public static StringBuilder MapIf(this StringBuilder builder, bool condition,
             Func<StringBuilder, StringBuilder> ifTrue)
         {
-            return condition ? ifTrue(builder) : builder;
+            return condition
+                ? ifTrue(builder)
+                : builder;
         }
 
         public static StringBuilder AppendIfNotEmpty(this StringBuilder builder, params string[] values)
         {
             foreach (var value in values)
-            {
                 if (value.Length > 0)
-                {
                     builder.Append(value);
-                }
-            }
+
             return builder;
         }
-        public static string SafeToString(this StringBuilder builder)        {            return builder == null ? string.Empty : builder.ToString();        }
+        public static string SafeToString(this StringBuilder builder)        {            return builder == null                ? string.Empty                : builder.ToString();        }
 
         public static int SafeLength(this StringBuilder builder)
         {
@@ -68,16 +66,19 @@ namespace CommandLine.Infrastructure
         public static StringBuilder TrimEnd(this StringBuilder builder, char c)
         {
             return builder.Length > 0
-                ? builder.Remove(builder.Length - 1, 1) : builder;
+                ? builder.Remove(builder.Length - 1, 1)
+                : builder;
         }
         public static StringBuilder TrimEndIfMatch(this StringBuilder builder, char c)        {
             if (builder.Length > 0)
                 if (builder[builder.Length - 1] == c)
-                    builder.Remove(builder.Length - 1, 1);            return builder;        }
+                    builder.Remove(builder.Length - 1, 1);            return builder;        }
 
         public static StringBuilder TrimEndIfMatchWhen(this StringBuilder builder, bool condition, char c)
         {
-            return condition ? builder.TrimEndIfMatch(c) : builder;
+            return condition
+                ? builder.TrimEndIfMatch(c)
+                : builder;
         }
         public static int TrailingSpaces(this StringBuilder builder)        {            var asString = builder.ToString();
             return asString.Length - asString.TrimEnd(' ').Length;        }    }
