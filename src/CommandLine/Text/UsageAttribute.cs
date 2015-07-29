f8ba44274f4af30e7531d9310901b2377d4fc459
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-07-29 07:35:36 +0200

    Adding Usage attribute draft

---
+++ src/CommandLine/Text/UsageAttribute.cs
@@ -0,0 +1,14 @@
+﻿// Copyright 2005-2015 Giacomo Stelluti Scala & Contributors. All rights reserved. See doc/License.md in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommandLine.Text
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public sealed class UsageAttribute : Attribute
+    {
+    }
+}
