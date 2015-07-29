566a7289712ada48dbb1b23a47979ef6c8c5bdf0
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-07-29 07:44:59 +0200

    Adding constructor

--- src/CommandLine/Text/Example.cs
+++ src/CommandLine/Text/Example.cs
@@ -5,7 +5,19 @@ using System.Text;
 
 namespace CommandLine.Text
 {
-    public sealed class Example
+    public sealed class Example<T>
     {
+        private readonly string group;
+        private readonly string helpText;
+        private readonly IEnumerable<UnParserSettings> settings;
+        private readonly T sample;
+
+        public Example(string group, string helpText, IEnumerable<UnParserSettings> settings, T sample)
+        {
+            this.group = group;
+            this.helpText = helpText;
+            this.settings = settings;
+            this.sample = sample;
+        }
     }
 }
