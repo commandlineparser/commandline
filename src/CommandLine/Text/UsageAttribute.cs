ac49f288380634ad90b3ef16a1ae812fdb0bbfe1
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-07-29 11:06:54 +0200

    Completing UsageAttribute with Groups map

--- src/CommandLine/Text/UsageAttribute.cs
+++ src/CommandLine/Text/UsageAttribute.cs
@@ -2,13 +2,27 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace CommandLine.Text
 {
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
     public sealed class UsageAttribute : Attribute
     {
+        private readonly IDictionary<string, string> groups;
+
+        public UsageAttribute(IDictionary<string, string> groups)
+        {
+            this.groups = groups;
+        }
+
+        public UsageAttribute()
+            : this(new Dictionary<string, string>())
+        {
+        }
+
+        public IDictionary<string, string> Groups
+        {
+            get { return groups; }
+        }
     }
 }
