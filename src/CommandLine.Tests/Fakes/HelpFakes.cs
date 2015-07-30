8bc7f5142bfcab42b070183290ee32f537838825
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-07-30 16:43:04 +0200

    Drafting verb fakes with usage

--- src/CommandLine.Tests/Fakes/HelpFakes.cs
+++ src/CommandLine.Tests/Fakes/HelpFakes.cs
@@ -69,4 +69,45 @@ namespace CommandLine.Tests.Fakes
             }
         }
     }
+
+    [Verb("add", HelpText = "Add file contents to the index.")]
+    public class AddOptionsWithUsage
+    {
+        [Option('p', "patch", SetName = "mode-p",
+            HelpText = "Interactively choose hunks of patch between the index and the work tree and add them to the index.")]
+        public bool Patch { get; set; }
+
+        [Option('f', "force", SetName = "mode-f",
+            HelpText = "Allow adding otherwise ignored files.")]
+        public bool Force { get; set; }
+
+        [Value(0)]
+        public string FileName { get; set; }
+    }
+
+    [Verb("commit", HelpText = "Record changes to the repository.")]
+    public class CommitOptionsWithUsage
+    {
+        [Option('p', "patch",
+            HelpText = "Use the interactive patch selection interface to chose which changes to commit.")]
+        public bool Patch { get; set; }
+
+        [Option("amend", HelpText = "Used to amend the tip of the current branch.")]
+        public bool Amend { get; set; }
+    }
+
+    [Verb("clone", HelpText = "Clone a repository into a new directory.")]
+    public class CloneOptionsWithUsage
+    {
+        [Option("no-hardlinks",
+            HelpText = "Optimize the cloning process from a repository on a local filesystem by copying files.")]
+        public bool NoHardLinks { get; set; }
+
+        [Option('q', "quiet",
+            HelpText = "Suppress summary message.")]
+        public bool Quiet { get; set; }
+
+        [Value(0)]
+        public IEnumerable<string> Urls { get; set; }
+    }
 }
