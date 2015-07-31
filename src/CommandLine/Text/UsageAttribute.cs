dbe9c82e71acfadaeff9cd0632cadf992ae16cd5
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-07-31 17:43:22 +0200

    Adding XML comments to UsageAttribute

--- src/CommandLine/Text/UsageAttribute.cs
+++ src/CommandLine/Text/UsageAttribute.cs
@@ -1,13 +1,19 @@
 ﻿// Copyright 2005-2015 Giacomo Stelluti Scala & Contributors. All rights reserved. See doc/License.md in the project root for license information.
 
 using System;
-using System.Collections.Generic;
 
 namespace CommandLine.Text
 {
+    /// <summary>
+    /// Applied to a static property that yields a sequence of <see cref="CommandLine.Text.Example"/>,
+    /// provides data to render usage section of help screen.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
     public sealed class UsageAttribute : Attribute
     {
+        /// <summary>
+        /// Application name, script or any means that starts current program.
+        /// </summary>
         public string ApplicationAlias { get; set; }
     }
 }
