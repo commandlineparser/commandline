7d67bb9b58b0d83142c2a74892def31a060b043c
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-08-02 17:39:57 +0200

    Adding FSharp.Formatting scripts

---
+++ docs/tools/generate.fsx
@@ -0,0 +1,95 @@
+// --------------------------------------------------------------------------------------
+// Builds the documentation from `.fsx` and `.md` files in the 'docs/content' directory
+// (the generated documentation is stored in the 'docs/output' directory)
+// --------------------------------------------------------------------------------------
+
+// Binaries that have XML documentation (in a corresponding generated XML file)
+let referenceBinaries = [ "net40/CommandLine.dll" ]
+// Web site location for the generated documentation
+let website = "/CommandLineParser"
+
+let githubLink = "http://github.com/gsscoder/commandline"
+
+// Specify more information about your project
+let info =
+  [ "project-name", "CommandLineParser"
+    "project-author", "Giacomo Stelluti Scala"
+    "project-summary", "Terse syntax C# command line parser for .NET with F# support"
+    "project-github", githubLink
+    "project-nuget", "http://www.nuget.org/packages/CommandLineParser" ]
+
+// --------------------------------------------------------------------------------------
+// For typical project, no changes are needed below
+// --------------------------------------------------------------------------------------
+
+#I "../../packages/FSharp.Compiler.Service/lib/net40"
+#I "../../packages/FSharpVSPowerTools.Core/lib/net45"
+#I "../../packages/FSharp.Formatting/lib/net40"
+#r "../../packages/FAKE/tools/FakeLib.dll"
+#r "FSharp.Markdown.dll"
+#r "FSharp.Literate.dll"
+#r "FSharp.CodeFormat.dll"
+#r "FSharp.MetadataFormat.dll"
+open Fake
+open System.IO
+open Fake.FileHelper
+open FSharp.Literate
+open FSharp.MetadataFormat
+
+// When called from 'build.fsx', use the public project URL as <root>
+// otherwise, use the current 'output' directory.
+#if RELEASE
+let root = website
+#else
+let root = "file://" + (__SOURCE_DIRECTORY__ @@ "../output")
+#endif
+
+// Paths with template/source/output locations
+let bin        = __SOURCE_DIRECTORY__ @@ "../../bin"
+let content    = __SOURCE_DIRECTORY__ @@ "../content"
+let output     = __SOURCE_DIRECTORY__ @@ "../output"
+let files      = __SOURCE_DIRECTORY__ @@ "../files"
+let templates  = __SOURCE_DIRECTORY__ @@ "templates"
+let formatting = __SOURCE_DIRECTORY__ @@ "../../packages/FSharp.Formatting/"
+let docTemplate = formatting @@ "templates/docpage.cshtml"
+
+// Where to look for *.csproj templates (in this order)
+let layoutRoots =
+  [ templates; formatting @@ "templates"
+    formatting @@ "templates/reference" ]
+
+// Copy static files and CSS + JS from F# Formatting
+let copyFiles () =
+  CopyRecursive files output true |> Log "Copying file: "
+  ensureDirectory (output @@ "content")
+  CopyRecursive (formatting @@ "styles") (output @@ "content") true 
+    |> Log "Copying styles and scripts: "
+
+// Build API reference from XML comments
+let buildReference () =
+  CleanDir (output @@ "reference")
+  let binaries =
+    referenceBinaries
+    |> List.map (fun lib-> bin @@ lib)
+  MetadataFormat.Generate
+    ( binaries, output @@ "reference", layoutRoots, 
+      parameters = ("root", root)::info,
+      sourceRepo = githubLink @@ "tree/master",
+      sourceFolder = __SOURCE_DIRECTORY__ @@ ".." @@ "..",
+      publicOnly = true )
+
+// Build documentation from `fsx` and `md` files in `docs/content`
+let buildDocumentation () =
+  let subdirs = Directory.EnumerateDirectories(content, "*", SearchOption.AllDirectories)
+  for dir in Seq.append [content] subdirs do
+    let sub = if dir.Length > content.Length then dir.Substring(content.Length + 1) else "."
+    Literate.ProcessDirectory
+      ( dir, docTemplate, output @@ sub, replacements = ("root", root)::info,
+        layoutRoots = layoutRoots, generateAnchors = true )
+
+// Generate
+CleanDir output
+CreateDir output
+copyFiles()
+buildDocumentation()
+buildReference()
