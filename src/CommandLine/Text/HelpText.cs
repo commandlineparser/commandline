bdb8f1d45628c1b6f3ff7f7b74e877786d4a3ee2
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-08-03 17:30:29 +0200

    Inverted if to reduce nesting

--- src/CommandLine/Text/HelpText.cs
+++ src/CommandLine/Text/HelpText.cs
@@ -907,17 +907,16 @@ namespace CommandLine.Text
                 return value.ToStringLocal();
 
             var asEnumerable = value as IEnumerable;
-            if (asEnumerable != null)
-            {
-                var builder = new StringBuilder();
-                foreach (var item in asEnumerable)
-                    builder
-                        .Append(item.ToStringLocal())
-                        .Append(" ");
+            if (asEnumerable == null)
+                return value.ToStringLocal();
 
-                return builder.Length > 0 ? builder.ToString(0, builder.Length - 1) : string.Empty;
-            }
-            return value.ToStringLocal();
+            var builder = new StringBuilder();
+            foreach (var item in asEnumerable)
+                builder
+                    .Append(item.ToStringLocal())
+                    .Append(" ");
+
+            return builder.Length > 0 ? builder.ToString(0, builder.Length - 1) : string.Empty;
         }
     }
 }
