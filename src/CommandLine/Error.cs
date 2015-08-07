2142cd109871a62603dadf668ff20c47b857dcac
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-08-07 19:01:23 +0200

    Fixing Error.GetHashCode() impl

--- src/CommandLine/Error.cs
+++ src/CommandLine/Error.cs
@@ -127,7 +127,7 @@ namespace CommandLine
         /// <remarks>A hash code for the current <see cref="System.Object"/>.</remarks>
         public override int GetHashCode()
         {
-            return Tag.GetHashCode();
+            return new { Tag, StopsProcessing }.GetHashCode();
         }
 
         /// <summary>
