1079d184f5a65f40c6cb0d171bf613c9a07eb95d
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-08-19 16:22:52 +0200

    Removed unused using stmts

--- tests/CommandLine.Tests/Unit/ParserResultExtensionsTests.cs
+++ tests/CommandLine.Tests/Unit/ParserResultExtensionsTests.cs
@@ -1,13 +1,7 @@
 ﻿// Copyright 2005-2015 Giacomo Stelluti Scala & Contributors. All rights reserved. See License.md in the project root for license information.
 
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using CommandLine.Tests.Fakes;
-
 using Xunit;
 using FluentAssertions;
 
