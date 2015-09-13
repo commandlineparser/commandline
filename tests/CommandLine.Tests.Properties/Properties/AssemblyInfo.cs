dab3fde20da1f0d98b45d7207707abade0dd5365
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-09-13 08:20:01 +0200

    Configured AssemblyInfo stuff

--- tests/CommandLine.Tests.Properties/Properties/AssemblyInfo.cs
+++ tests/CommandLine.Tests.Properties/Properties/AssemblyInfo.cs
@@ -1,36 +1,25 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿// Copyright 2005-2015 Giacomo Stelluti Scala & Contributors. All rights reserved. See License.md in the project root for license information.
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("CommandLine.Tests.Properties")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CommandLine.Tests.Properties")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
+using System.Reflection;
+using System.Runtime.InteropServices;
+using CommandLine.Text;
+
+[assembly: AssemblyTitle("CommandLine.Tests.Properties.dll")]
+[assembly: AssemblyDescription("Command Line Parser Library allows CLR applications to define a syntax for parsing command line arguments.")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyLicense(
+    "This is free software. You may redistribute copies of it under the terms of",
+    "the MIT License <http://www.opensource.org/licenses/mit-license.php>.")]
+[assembly: AssemblyUsage(
+    "[no usage, this is a dll]")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+//[assembly: AssemblyConfiguration("Release")]
+#endif
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a917f20d-fc6d-4eb1-bdf0-f6c71300c5bd")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: CLSCompliant(true)]
+//[assembly: AssemblyCompany("")]
+//[assembly: AssemblyTrademark("")]
