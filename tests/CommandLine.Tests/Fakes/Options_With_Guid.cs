3977edf9616496c50f60b113f3560e5289c00a98
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-09-22 16:13:26 +0200

    Added fake class for test for Issue #241

---
+++ tests/CommandLine.Tests/Fakes/Options_With_Guid.cs
@@ -0,0 +1,12 @@
+﻿// Copyright 2005-2015 Giacomo Stelluti Scala & Contributors. All rights reserved. See License.md in the project root for license information.
+
+using System;
+
+namespace CommandLine.Tests.Fakes
+{
+    class Options_With_Guid
+    {
+        [Option]
+        public Guid TransactionId { get; set; }
+    }
+}
