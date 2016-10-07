9146a74175717c5ff32dfb3f5a66cdb5fd129498
Anthony Langsworth <anthony.langsworth@gmail.com>
2016-10-07 22:58:22 +1100

    Expand tests

--- tests/CommandLine.Tests/Unit/Core/TypeConverterTests.cs
+++ tests/CommandLine.Tests/Unit/Core/TypeConverterTests.cs
@@ -17,19 +17,19 @@ namespace CommandLine.Tests.Unit.Core
     {
         [Theory]
         [MemberData("ChangeType_scalars_source")]
-        public void ChangeType_scalars(string testValue, Type destinationType, CultureInfo culture, bool expectFail, object expectedResult)
+        public void ChangeType_scalars(string testValue, Type destinationType, bool expectFail, object expectedResult)
         {
-            Maybe<object> result = TypeConverter.ChangeType(new[] {testValue}, destinationType, true, culture, true);
+            Maybe<object> result = TypeConverter.ChangeType(new[] {testValue}, destinationType, true, CultureInfo.InvariantCulture, true);
 
             if (expectFail)
             {
-                result.MatchNothing().Should().BeTrue();
+                result.MatchNothing().Should().BeTrue("should fail parsing");
             }
             else
             {
                 object matchedValue;
 
-                result.MatchJust(out matchedValue).Should().BeTrue();
+                result.MatchJust(out matchedValue).Should().BeTrue("should parse successfully");
                 Assert.Equal(matchedValue, expectedResult);
             }
         }
@@ -38,29 +38,56 @@ namespace CommandLine.Tests.Unit.Core
         {
             get
             {
-                yield return new object[] { "1", typeof (int), CultureInfo.InvariantCulture, false, 1};
-                yield return new object[] { "0", typeof (int), CultureInfo.InvariantCulture, false, 0 };
-                yield return new object[] { "-1", typeof (int), CultureInfo.InvariantCulture, false, -1 };
-                yield return new object[] { "1.0", typeof (int), CultureInfo.InvariantCulture, true, null };
-                yield return new object[] { "1.0", typeof(float), CultureInfo.InvariantCulture, false, 1.0f };
-                yield return new object[] { "0.0", typeof (float), CultureInfo.InvariantCulture, false, 0.0f};
-                yield return new object[] { "-1.0", typeof (float), CultureInfo.InvariantCulture, false, -1.0f};
-                yield return new object[] { "1.0", typeof(double), CultureInfo.InvariantCulture, false, 1.0 };
-                yield return new object[] { "0.0", typeof (double), CultureInfo.InvariantCulture, false, 0.0};
-                yield return new object[] { "-1.0", typeof(double), CultureInfo.InvariantCulture, false, -1.0 };
-                yield return new object[] { "1.0", typeof(decimal), CultureInfo.InvariantCulture, false, 1.0m };
-                yield return new object[] { "0.0", typeof(decimal), CultureInfo.InvariantCulture, false, 0.0m };
-                yield return new object[] { "-1.0", typeof(decimal), CultureInfo.InvariantCulture, false, -1.0m };
-                yield return new object[] { "-1.123456", typeof(decimal), CultureInfo.InvariantCulture, false, -1.123456m };
-                yield return new object[] { "true", typeof(bool), CultureInfo.InvariantCulture, false, true };
-                yield return new object[] { "false", typeof (bool), CultureInfo.InvariantCulture, false, false };
-                yield return new object[] { "", typeof(string), CultureInfo.InvariantCulture, false, "" };
-                yield return new object[] { "abcd", typeof(int), CultureInfo.InvariantCulture, true, "abcd" };
-                yield return new object[] { "abcd", typeof(string), CultureInfo.InvariantCulture, false, "abcd" };
+                yield return new object[] { "1", typeof (int), false, 1};
+                yield return new object[] { "0", typeof (int), false, 0 };
+                yield return new object[] { "-1", typeof (int), false, -1 };
+                yield return new object[] { "abcd", typeof(int), true, null };
+                yield return new object[] { "1.0", typeof(int), true, null };
+                yield return new object[] { int.MaxValue.ToString(), typeof(int), false, int.MaxValue };
+                yield return new object[] { int.MinValue.ToString(), typeof(int), false, int.MinValue };
+                yield return new object[] { ((long)int.MaxValue + 1).ToString(), typeof(int), true, null };
+                yield return new object[] { ((long)int.MinValue - 1).ToString(), typeof(int), true, null };
+
+                yield return new object[] { "1", typeof(uint), false, (uint) 1 };
+                yield return new object[] { "0", typeof(uint), false, (uint) 0 };
+                yield return new object[] { "-1", typeof(uint), true, null };
+                yield return new object[] { uint.MaxValue.ToString(), typeof(uint), false, uint.MaxValue };
+                yield return new object[] { uint.MinValue.ToString(), typeof(uint), false, uint.MinValue };
+                yield return new object[] { ((long)uint.MaxValue + 1).ToString(), typeof(uint), true, null };
+                yield return new object[] { ((long)uint.MinValue - 1).ToString(), typeof(uint), true, null };
+
+                yield return new object[] { "true", typeof(bool), false, true };
+                yield return new object[] { "True", typeof(bool), false, true };
+                yield return new object[] { "TRUE", typeof(bool), false, true };
+                yield return new object[] { "false", typeof(bool), false, false };
+                yield return new object[] { "False", typeof(bool), false, false };
+                yield return new object[] { "FALSE", typeof(bool), false, false };
+                yield return new object[] { "abcd", typeof(bool), true, null };
+                yield return new object[] { "0", typeof(bool), true, null };
+                yield return new object[] { "1", typeof(bool), true, null };
+
+                yield return new object[] { "1.0", typeof(float), false, 1.0f };
+                yield return new object[] { "0.0", typeof (float), false, 0.0f};
+                yield return new object[] { "-1.0", typeof (float), false, -1.0f};
+                yield return new object[] { "abcd", typeof(float), true, null };
+
+                yield return new object[] { "1.0", typeof(double), false, 1.0 };
+                yield return new object[] { "0.0", typeof (double), false, 0.0};
+                yield return new object[] { "-1.0", typeof(double), false, -1.0 };
+                yield return new object[] { "abcd", typeof(double), true, null };
+
+                yield return new object[] { "1.0", typeof(decimal), false, 1.0m };
+                yield return new object[] { "0.0", typeof(decimal), false, 0.0m };
+                yield return new object[] { "-1.0", typeof(decimal), false, -1.0m };
+                yield return new object[] { "-1.123456", typeof(decimal), false, -1.123456m };
+                yield return new object[] { "abcd", typeof(decimal), true, null };
+
+                yield return new object[] { "", typeof(string), false, "" };
+                yield return new object[] { "abcd", typeof(string), false, "abcd" };
 
                 // Failed before change
-                yield return new object[] { "false", typeof(int), CultureInfo.InvariantCulture, true, 0 };
-                yield return new object[] { "true", typeof(int), CultureInfo.InvariantCulture, true, 0 };
+                yield return new object[] { "false", typeof(int), true, 0 };
+                yield return new object[] { "true", typeof(int), true, 0 };
             }
         }
     }
