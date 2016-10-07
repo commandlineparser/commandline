977b7546ec53b77c57e08c81a4227ee65486a4d2
Anthony Langsworth <anthony.langsworth@gmail.com>
2016-10-07 23:02:11 +1100

    Convert from yield return to object[][]

--- tests/CommandLine.Tests/Unit/Core/TypeConverterTests.cs
+++ tests/CommandLine.Tests/Unit/Core/TypeConverterTests.cs
@@ -38,56 +38,59 @@ namespace CommandLine.Tests.Unit.Core
         {
             get
             {
-                yield return new object[] { "1", typeof (int), false, 1};
-                yield return new object[] { "0", typeof (int), false, 0 };
-                yield return new object[] { "-1", typeof (int), false, -1 };
-                yield return new object[] { "abcd", typeof(int), true, null };
-                yield return new object[] { "1.0", typeof(int), true, null };
-                yield return new object[] { int.MaxValue.ToString(), typeof(int), false, int.MaxValue };
-                yield return new object[] { int.MinValue.ToString(), typeof(int), false, int.MinValue };
-                yield return new object[] { ((long)int.MaxValue + 1).ToString(), typeof(int), true, null };
-                yield return new object[] { ((long)int.MinValue - 1).ToString(), typeof(int), true, null };
+                return new[]
+                {
+                    new object[] {"1", typeof (int), false, 1},
+                    new object[] {"0", typeof (int), false, 0},
+                    new object[] {"-1", typeof (int), false, -1},
+                    new object[] {"abcd", typeof (int), true, null},
+                    new object[] {"1.0", typeof (int), true, null},
+                    new object[] {int.MaxValue.ToString(), typeof (int), false, int.MaxValue},
+                    new object[] {int.MinValue.ToString(), typeof (int), false, int.MinValue},
+                    new object[] {((long) int.MaxValue + 1).ToString(), typeof (int), true, null},
+                    new object[] {((long) int.MinValue - 1).ToString(), typeof (int), true, null},
 
-                yield return new object[] { "1", typeof(uint), false, (uint) 1 };
-                yield return new object[] { "0", typeof(uint), false, (uint) 0 };
-                yield return new object[] { "-1", typeof(uint), true, null };
-                yield return new object[] { uint.MaxValue.ToString(), typeof(uint), false, uint.MaxValue };
-                yield return new object[] { uint.MinValue.ToString(), typeof(uint), false, uint.MinValue };
-                yield return new object[] { ((long)uint.MaxValue + 1).ToString(), typeof(uint), true, null };
-                yield return new object[] { ((long)uint.MinValue - 1).ToString(), typeof(uint), true, null };
+                    new object[] {"1", typeof (uint), false, (uint) 1},
+                    new object[] {"0", typeof (uint), false, (uint) 0},
+                    new object[] {"-1", typeof (uint), true, null},
+                    new object[] {uint.MaxValue.ToString(), typeof (uint), false, uint.MaxValue},
+                    new object[] {uint.MinValue.ToString(), typeof (uint), false, uint.MinValue},
+                    new object[] {((long) uint.MaxValue + 1).ToString(), typeof (uint), true, null},
+                    new object[] {((long) uint.MinValue - 1).ToString(), typeof (uint), true, null},
 
-                yield return new object[] { "true", typeof(bool), false, true };
-                yield return new object[] { "True", typeof(bool), false, true };
-                yield return new object[] { "TRUE", typeof(bool), false, true };
-                yield return new object[] { "false", typeof(bool), false, false };
-                yield return new object[] { "False", typeof(bool), false, false };
-                yield return new object[] { "FALSE", typeof(bool), false, false };
-                yield return new object[] { "abcd", typeof(bool), true, null };
-                yield return new object[] { "0", typeof(bool), true, null };
-                yield return new object[] { "1", typeof(bool), true, null };
+                    new object[] {"true", typeof (bool), false, true},
+                    new object[] {"True", typeof (bool), false, true},
+                    new object[] {"TRUE", typeof (bool), false, true},
+                    new object[] {"false", typeof (bool), false, false},
+                    new object[] {"False", typeof (bool), false, false},
+                    new object[] {"FALSE", typeof (bool), false, false},
+                    new object[] {"abcd", typeof (bool), true, null},
+                    new object[] {"0", typeof (bool), true, null},
+                    new object[] {"1", typeof (bool), true, null},
 
-                yield return new object[] { "1.0", typeof(float), false, 1.0f };
-                yield return new object[] { "0.0", typeof (float), false, 0.0f};
-                yield return new object[] { "-1.0", typeof (float), false, -1.0f};
-                yield return new object[] { "abcd", typeof(float), true, null };
+                    new object[] {"1.0", typeof (float), false, 1.0f},
+                    new object[] {"0.0", typeof (float), false, 0.0f},
+                    new object[] {"-1.0", typeof (float), false, -1.0f},
+                    new object[] {"abcd", typeof (float), true, null},
 
-                yield return new object[] { "1.0", typeof(double), false, 1.0 };
-                yield return new object[] { "0.0", typeof (double), false, 0.0};
-                yield return new object[] { "-1.0", typeof(double), false, -1.0 };
-                yield return new object[] { "abcd", typeof(double), true, null };
+                    new object[] {"1.0", typeof (double), false, 1.0},
+                    new object[] {"0.0", typeof (double), false, 0.0},
+                    new object[] {"-1.0", typeof (double), false, -1.0},
+                    new object[] {"abcd", typeof (double), true, null},
 
-                yield return new object[] { "1.0", typeof(decimal), false, 1.0m };
-                yield return new object[] { "0.0", typeof(decimal), false, 0.0m };
-                yield return new object[] { "-1.0", typeof(decimal), false, -1.0m };
-                yield return new object[] { "-1.123456", typeof(decimal), false, -1.123456m };
-                yield return new object[] { "abcd", typeof(decimal), true, null };
+                    new object[] {"1.0", typeof (decimal), false, 1.0m},
+                    new object[] {"0.0", typeof (decimal), false, 0.0m},
+                    new object[] {"-1.0", typeof (decimal), false, -1.0m},
+                    new object[] {"-1.123456", typeof (decimal), false, -1.123456m},
+                    new object[] {"abcd", typeof (decimal), true, null},
 
-                yield return new object[] { "", typeof(string), false, "" };
-                yield return new object[] { "abcd", typeof(string), false, "abcd" };
+                    new object[] {"", typeof (string), false, ""},
+                    new object[] {"abcd", typeof (string), false, "abcd"},
 
-                // Failed before change
-                yield return new object[] { "false", typeof(int), true, 0 };
-                yield return new object[] { "true", typeof(int), true, 0 };
+                    // Failed before #339
+                    new object[] {"false", typeof (int), true, 0},
+                    new object[] {"true", typeof (int), true, 0}
+                };
             }
         }
     }
