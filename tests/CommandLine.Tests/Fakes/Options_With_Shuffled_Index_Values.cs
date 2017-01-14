719571cd3fb311992727fd62996d26bf6a141502
zii-dmg <master-chi@mail.ru>
2017-01-14 13:53:55 +0300

    User [Value] index when parsing
    Fixes value parsing depending on properties order.

---
+++ tests/CommandLine.Tests/Fakes/Options_With_Shuffled_Index_Values.cs
@@ -0,0 +1,19 @@
+﻿// Copyright 2005-2015 Giacomo Stelluti Scala & Contributors. All rights reserved. See License.md in the project root for license information.
+
+using System;
+using System.Linq;
+
+namespace CommandLine.Tests.Fakes
+{
+    class Options_With_Shuffled_Index_Values
+    {
+        [Value(1)]
+        public string Arg1 { get; set; }
+
+        [Value(2)]
+        public string Arg2 { get; set; }
+
+        [Value(0)]
+        public string Arg0 { get; set; }
+    }
+}
