33ce7b98d7d18d42cc3da1e468068a9077ff6b92
Eric Newton <ericnewton76@gmail.com>
2018-01-06 11:54:50 -0500

    updates for release prep

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -1,13 +1,13 @@
-// Copyright 2005-2015 Giacomo Stelluti Scala & Contributors. All rights reserved. See License.md in the project root for license information.
+// Copyright 2005-2018 Giacomo Stelluti Scala & Contributors. All rights reserved. See License.md in the project root for license information.
 
 using System.Reflection;
 using System.Resources;
 
 [assembly: AssemblyProduct("Command Line Parser Library")]
-[assembly: AssemblyCopyright("Copyright (c) 2005 - 2015 Giacomo Stelluti Scala")]
+[assembly: AssemblyCopyright("Copyright (c) 2005 - 2018 Giacomo Stelluti Scala & Contributors")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
 // versions are replaced during CI build
-[assembly: AssemblyVersion("2.0.275.0")]
-[assembly: AssemblyFileVersion("2.0.275.0")]
-[assembly: AssemblyInformationalVersion("2.0.275-beta")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyInformationalVersion("2.2.0.0")]
