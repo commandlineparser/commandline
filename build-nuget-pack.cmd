9f78de8635fc07471249644a19f025999434d24b
Eric Newton <ericnewton76@gmail.com>
2018-01-10 18:33:12 -0500

    appveyor build updates

--- build-nuget-pack.cmd
+++ build-nuget-pack.cmd
@@ -2,16 +2,18 @@
 setlocal
 
 if "%1" == "" goto :USAGE
+if "%2" == "" goto :USAGE
 
 pushd Release\%1
 
 copy ..\..\README.md
-copy ..\..\CommandLine.%1.nuspec .
-nuget pack "CommandLine.%1.nuspec" -properties Version=%APPVEYOR_BUILD_VERSION%
+copy ..\..\%2.nuspec .
+nuget pack "%2.nuspec" -properties Version=%APPVEYOR_BUILD_VERSION%
 if errorlevel 1 popd&exit 1 /b
 goto :END
 
 :USAGE
+echo build-nuget-pack <build_target> <nuspec_file_name>
 
 :END
 popd
