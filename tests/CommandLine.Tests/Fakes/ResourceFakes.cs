589869dd6273ca545f8c73c5a3ef120f26ccc1fc
Tomas Arci Kouba <tomas.kouba@gmail.com>
2018-10-09 00:53:23 +0200

    Fakes without expression bodied properties

--- tests/CommandLine.Tests/Fakes/ResourceFakes.cs
+++ tests/CommandLine.Tests/Fakes/ResourceFakes.cs
@@ -8,24 +8,24 @@ namespace CommandLine.Tests.Fakes
 {
     public static class StaticResource
     {
-        public static string HelpText { get => "Localized HelpText"; }
+        public static string HelpText { get { return "Localized HelpText"; } }
     }
 
     public class NonStaticResource
     {
-        public static string HelpText { get => "Localized HelpText"; }
-        public static string WriteOnlyText { set => value?.ToString(); }
-        private static string PrivateHelpText { get => "Localized HelpText"; }
+        public static string HelpText { get { return "Localized HelpText"; } }
+        public static string WriteOnlyText { set { value?.ToString(); } }
+        private static string PrivateHelpText { get { return "Localized HelpText"; } }
     }
 
     public class NonStaticResource_WithNonStaticProperty
     {
-        public string HelpText { get => "Localized HelpText"; }
+        public string HelpText { get { return "Localized HelpText"; } }
     }
 
     internal class InternalResource
     {
-        public static string HelpText { get => "Localized HelpText"; }
+        public static string HelpText { get { return "Localized HelpText"; } }
     }
 
 }
