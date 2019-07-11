4703c74e0ee25b8b364918f0bc5300cd8fb98e03
Moh.Hassan <moh.hassan20@gmail.com>
2019-07-11 22:10:42 +0200

    Add errs.Output extension method to auto direct help/version to Console.Out and errors to Console.Error

--- src/CommandLine/HelpTextExtensions.cs
+++ src/CommandLine/HelpTextExtensions.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright 2005-2015 Giacomo Stelluti Scala & Contributors. All rights reserved. See License.md in the project root for license information.
-
+using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -28,6 +29,15 @@ namespace CommandLine
                 return true;
             //when  AutoVersion=false in parser, Version is disabled  and Parser raise UnknownOptionError
             return errs.Any(x => (x is UnknownOptionError ee ? ee.Token : "") == "version");
+        }
+		 /// <summary>
+        ///  redirect errs to Console.Error, and to Console.Out for help/version error
+        /// </summary>
+		 public static TextWriter Output(this IEnumerable<Error> errs)
+        {
+           if (errs.IsHelp() || errs.IsVersion())
+			   return Console.Out;
+		   return Console.Error;
         }
     }
 }
