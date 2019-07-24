7ede4da79e68386a1f85a13842bc68b0683e8645
Tomas Arci Kouba <tomas.kouba@gmail.com>
2019-07-24 21:49:53 +0200

    Localized demo

---
+++ demo/ReadText.LocalizedDemo/LocalizableSentenceBuilder.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommandLine;
+using CommandLine.Text;
+
+namespace ReadText.LocalizedDemo
+{
+    public class LocalizableSentenceBuilder : SentenceBuilder
+    {
+        public override Func<string> RequiredWord
+        {
+            get { return () => Properties.Resources.SentenceRequiredWord; }
+        }
+
+        public override Func<string> ErrorsHeadingText
+        {
+            // Cannot be pluralized
+            get { return () => Properties.Resources.SentenceErrorsHeadingText; }
+        }
+
+        public override Func<string> UsageHeadingText
+        {
+            get { return () => Properties.Resources.SentenceUsageHeadingText; }
+        }
+
+        public override Func<bool, string> HelpCommandText
+        {
+            get
+            {
+                return isOption => isOption
+                    ? Properties.Resources.SentenceHelpCommandTextOption
+                    : Properties.Resources.SentenceHelpCommandTextVerb;
+            }
+        }
+
+        public override Func<bool, string> VersionCommandText
+        {
+            get { return _ => Properties.Resources.SentenceVersionCommandText; }
+        }
+
+        public override Func<Error, string> FormatError
+        {
+            get
+            {
+                return error =>
+                {
+                    switch (error.Tag)
+                    {
+                        case ErrorType.BadFormatTokenError:
+                            return String.Format(Properties.Resources.SentenceBadFormatTokenError, ((BadFormatTokenError)error).Token);
+                        case ErrorType.MissingValueOptionError:
+                            return String.Format(Properties.Resources.SentenceMissingValueOptionError, ((MissingValueOptionError)error).NameInfo.NameText);
+                        case ErrorType.UnknownOptionError:
+                            return String.Format(Properties.Resources.SentenceUnknownOptionError, ((UnknownOptionError)error).Token);
+                        case ErrorType.MissingRequiredOptionError:
+                            var errMisssing = ((MissingRequiredOptionError)error);
+                            return errMisssing.NameInfo.Equals(NameInfo.EmptyName)
+                                       ? Properties.Resources.SentenceMissingRequiredOptionError
+                                       : String.Format(Properties.Resources.SentenceMissingRequiredOptionError, errMisssing.NameInfo.NameText);
+                        case ErrorType.BadFormatConversionError:
+                            var badFormat = ((BadFormatConversionError)error);
+                            return badFormat.NameInfo.Equals(NameInfo.EmptyName)
+                                       ? Properties.Resources.SentenceBadFormatConversionErrorValue
+                                       : String.Format(Properties.Resources.SentenceBadFormatConversionErrorOption, badFormat.NameInfo.NameText);
+                        case ErrorType.SequenceOutOfRangeError:
+                            var seqOutRange = ((SequenceOutOfRangeError)error);
+                            return seqOutRange.NameInfo.Equals(NameInfo.EmptyName)
+                                       ? Properties.Resources.SentenceSequenceOutOfRangeErrorValue
+                                       : String.Format(Properties.Resources.SentenceSequenceOutOfRangeErrorOption, 
+                                            seqOutRange.NameInfo.NameText);
+                        case ErrorType.BadVerbSelectedError:
+                            return String.Format(Properties.Resources.SentenceBadVerbSelectedError, ((BadVerbSelectedError)error).Token);
+                        case ErrorType.NoVerbSelectedError:
+                            return Properties.Resources.SentenceNoVerbSelectedError;
+                        case ErrorType.RepeatedOptionError:
+                            return String.Format(Properties.Resources.SentenceRepeatedOptionError, ((RepeatedOptionError)error).NameInfo.NameText);
+                        case ErrorType.SetValueExceptionError:
+                            var setValueError = (SetValueExceptionError)error;
+                            return String.Format(Properties.Resources.SentenceSetValueExceptionError, setValueError.NameInfo.NameText, setValueError.Exception.Message);
+                    }
+                    throw new InvalidOperationException();
+                };
+            }
+        }
+
+        public override Func<IEnumerable<MutuallyExclusiveSetError>, string> FormatMutuallyExclusiveSetErrors
+        {
+            get
+            {
+                return errors =>
+                {
+                    var bySet = from e in errors
+                                group e by e.SetName into g
+                                select new { SetName = g.Key, Errors = g.ToList() };
+
+                    var msgs = bySet.Select(
+                        set =>
+                        {
+                            var names = String.Join(
+                                String.Empty,
+                                (from e in set.Errors select String.Format("'{0}', ", e.NameInfo.NameText)).ToArray());
+                            var namesCount = set.Errors.Count();
+
+                            var incompat = String.Join(
+                                String.Empty,
+                                (from x in
+                                     (from s in bySet where !s.SetName.Equals(set.SetName) from e in s.Errors select e)
+                                    .Distinct()
+                                 select String.Format("'{0}', ", x.NameInfo.NameText)).ToArray());
+                            //TODO: Pluralize by namesCount
+                            return
+                                String.Format(Properties.Resources.SentenceMutuallyExclusiveSetErrors,
+                                    names.Substring(0, names.Length - 2), incompat.Substring(0, incompat.Length - 2));
+                        }).ToArray();
+                    return string.Join(Environment.NewLine, msgs);
+                };
+            }
+        }
+    }
+}
