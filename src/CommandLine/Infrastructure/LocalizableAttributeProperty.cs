a50eb409691e560a0fdbae04e83c61e76085ca66
edmondshtogu <edmondshtogu@gmail.com>
2020-01-04 23:20:00 +0100

    more details for localized attribute properties...
    If the resources are not visible the parser library throws a useless exception.
    These extra details help with finding the issue quicker.

--- src/CommandLine/Infrastructure/LocalizableAttributeProperty.cs
+++ src/CommandLine/Infrastructure/LocalizableAttributeProperty.cs
@@ -45,10 +45,10 @@ namespace CommandLine.Infrastructure
             {
                 // Static class IsAbstract 
                 if (!_type.IsVisible)
-                    throw new ArgumentException("Invalid resource type", _propertyName);
+                    throw new ArgumentException($"Invalid resource type '{_type.FullName}'! {_type.Name} is not visible for the parser! Change resources 'Access Modifier' to 'Public'", _propertyName);
                 PropertyInfo propertyInfo = _type.GetProperty(_value, BindingFlags.Public | BindingFlags.GetProperty | BindingFlags.Static);
                 if (propertyInfo == null || !propertyInfo.CanRead || propertyInfo.PropertyType != typeof(string))
-                    throw new ArgumentException("Invalid resource property name", _propertyName);
+                    throw new ArgumentException("Invalid resource property name! Localized value: {_value}", _propertyName);
                 _localizationPropertyInfo = propertyInfo;
             }
             return (string)_localizationPropertyInfo.GetValue(null, null);
