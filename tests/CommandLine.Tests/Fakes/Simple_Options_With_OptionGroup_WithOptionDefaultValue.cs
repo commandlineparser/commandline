32bbe0004875bffff270ee02ce9b6e0a2564213b
Vladislav Hadzhiyski <vladislav.hadzhiyski@gmail.com>
2020-02-02 20:02:04 +0200

    Fix option groups (#575)
    * Add MyGet package provider to consume the daily builds. (#566)
    
    * Options groups take in account default value
    
    * Do not allow options groups and exclusive set names to be used together
    
    * Multiple group errors are shown together
    
    * MissingGroupOptionError compare option names
    
    Co-authored-by: Mohamed Hassan <moh.hassan20@gmail.com>

---
+++ tests/CommandLine.Tests/Fakes/Simple_Options_With_OptionGroup_WithOptionDefaultValue.cs
@@ -0,0 +1,14 @@
+﻿namespace CommandLine.Tests.Fakes
+{
+    public class Simple_Options_With_OptionGroup_WithOptionDefaultValue
+    {
+        [Option(HelpText = "Define a string value here.", Required = true, Group = "test", Default = "qwerty123")]
+        public string StringValue { get; set; }
+
+        [Option('s', "shortandlong", HelpText = "Example with both short and long name.", Required = true, Group = "test")]
+        public string ShortAndLong { get; set; }
+
+        [Option('x', HelpText = "Define a boolean or switch value here.")]
+        public bool BoolValue { get; set; }
+    }
+}
