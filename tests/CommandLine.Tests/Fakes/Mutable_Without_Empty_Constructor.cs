4698674fadbf3298d85df7091773e2a0005fff3f
Per Gårdebrink <per.gardebrink@greenbyte.com>
2020-03-08 13:07:43 +0100

    Add test for #70

---
+++ tests/CommandLine.Tests/Fakes/Mutable_Without_Empty_Constructor.cs
@@ -0,0 +1,19 @@
+﻿// Copyright 2005-2015 Giacomo Stelluti Scala & Contributors. All rights reserved. See License.md in the project root for license information.
+
+namespace CommandLine.Tests.Fakes 
+{
+    class Mutable_Without_Empty_Constructor 
+    {
+        [Option("amend", HelpText = "Used to amend the tip of the current branch.")]
+        public bool Amend { get; set; }
+
+        private Mutable_Without_Empty_Constructor() 
+        {
+        }
+
+        public static Mutable_Without_Empty_Constructor Create() 
+        {
+            return new Mutable_Without_Empty_Constructor();
+        }
+    }
+}
