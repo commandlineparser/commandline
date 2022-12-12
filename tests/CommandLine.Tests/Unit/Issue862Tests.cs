bc2659da76d724a02df463c5c67816065831588c
Orace <pierre.lando@gmail.com>
2022-12-12 11:55:45 +0100

    Issue862. Add failing test.

---
+++ tests/CommandLine.Tests/Unit/Issue862Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace CommandLine.Tests.Unit
+{
+    #if NET7_0_OR_GREATER
+    public class Issue862Tests
+    {
+
+        [Fact]
+        public void Properties_with_required_modifier_are_required()
+        {
+            var arguments = Array.Empty<string>();
+            var result = new Parser().ParseArguments<Options>(arguments);
+
+            result.Errors.Should().ContainSingle(e => e.Tag == ErrorType.MissingRequiredOptionError);
+            result.Tag.Should().Be(ParserResultType.NotParsed);
+        }
+
+        private class Options
+        {
+            [Option("cols")]
+            public required int Cols { get; set; }
+        }
+    }
+
+    #endif
+}
