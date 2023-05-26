87db170ee6915cdcf03d9261e60c7a2e40eaec08
phani vanka <phani_va@yahoo.com>
2023-05-26 19:58:28 -0400

    add missing fake for testing Dictionay specification type

---
+++ tests/CommandLine.Tests/Fakes/Options_With_Dictionary_Specification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLine.Tests.Fakes
+{
+    class Options_With_Dictionary_Specification
+    {
+        [Option('d', "dict")]
+        public Dictionary<string, string> KeyValuePairs { get; set; }
+    }
+}
