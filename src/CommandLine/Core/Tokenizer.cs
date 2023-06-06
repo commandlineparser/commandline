9be1ee811bfc496166b41d8226d6417b407307e4
Andrii Kurdiumov <kant2002@gmail.com>
2023-06-06 15:50:22 +0600

    Fix parsing values which starts with dash, but has whitespace inside. this is required when your option can accept command line arguments to other applications.
    See https://github.com/dotnet/BenchmarkDotNet/pull/2320#issuecomment-1577300888 for example of parsing issues

--- src/CommandLine/Core/Tokenizer.cs
+++ src/CommandLine/Core/Tokenizer.cs
@@ -28,7 +28,7 @@ namespace CommandLine.Core
             Action<Error> onError = errors.Add;
 
             var tokens = (from arg in arguments
-                          from token in !arg.StartsWith("-", StringComparison.Ordinal)
+                          from token in (!arg.StartsWith("-", StringComparison.Ordinal) || arg.Contains(" "))
                                ? new[] { Token.Value(arg) }
                                : arg.StartsWith("--", StringComparison.Ordinal)
                                      ? TokenizeLongName(arg, onError)
